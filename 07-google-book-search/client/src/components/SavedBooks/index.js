ebb5e2771e1f4e40f593e253385f6fcc9eb77ee4
jsp <jonsp14@gmail.com>
2020-11-18 12:39:01 -0800

    fix image display on saved books

--- 07-google-book-search/client/src/components/SavedBooks/index.js
+++ 07-google-book-search/client/src/components/SavedBooks/index.js
@@ -2,27 +2,30 @@ import React from "react";
 // import booksAPI from "../../utils/booksAPI";
 import "./style.css";
 
-function SavedBooks({books, deleteBooks}) {
- 
+function SavedBooks({ books, deleteBooks }) {
+  console.log("books", books);
   return (
 
     books.map(book => {
-      return(
+      return (
         <div className="container">
           <div className="card">
-              <div className="row mb-4 mt-3">
-                <div className="col-lg-4 bookImg">
-                  <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid"/>
-                  </div>
-                  <div className="col-lg-8">
-                    <h2>{book.title}</h2>
-                    <p>{book.author}</p>
-                    <p>{book.synopsis}</p>
-                    <a onClick={() => deleteBooks(book._id)} className="col-sm-1 deleteBtn">X</a>
-                    <button className="btn btn-primary">VIEW</button>
-                  </div>
+            <div className="row mb-4 mt-3">
+              <div className="col-lg-4 bookImg">
+                {/* <img src={book.image} alt="title" className="img-fluid" /> */}
+                {book.image ? <img src={book.image} alt="title" className="img-fluid" /> 
+                  : <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid" />}
+                {/* <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid" /> */}
+              </div>
+              <div className="col-lg-8">
+                <h2>{book.title}</h2>
+                <p>{book.author}</p>
+                <p>{book.synopsis}</p>
+                <a onClick={() => deleteBooks(book._id)} className="col-sm-1 deleteBtn">X</a>
+                <button className="btn btn-primary">VIEW</button>
               </div>
             </div>
+          </div>
         </div>
       )
     })
@@ -31,7 +34,7 @@ function SavedBooks({books, deleteBooks}) {
 
 export default SavedBooks;
 
-  {/* <div className="padding"></div>
+{/* <div className="padding"></div>
           <h2>{books.title}</h2>
           <p>{books.author}</p>
           <p>{books.description}</p>
@@ -45,17 +48,17 @@ export default SavedBooks;
 
        //   <div className="card">
   //       <div className="row">
-        
+
   //       {books.map((book)=>{
   //          return <div key={book._id} className="container">
   //          <div className="row">
   //              <div className="col-sm-4"></div>               
   //              <h5 className="col-sm-3">{book.title + " "}</h5>
-         
+
   //          </div>
   //          </div>;
   //       })}
-      
+
   //       </div>
   //   </div>
   // );
