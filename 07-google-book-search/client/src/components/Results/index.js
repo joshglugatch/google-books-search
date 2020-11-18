c4635986e518caa82b2fd5153a5a91c3f703a5f5
Josh Glugatch <joshglugatch@gmail.com>
2020-11-18 11:03:18 -0800

    map results on search

--- 07-google-book-search/client/src/components/Results/index.js
+++ 07-google-book-search/client/src/components/Results/index.js
@@ -1,21 +1,30 @@
 import React from "react";
 
 function Results(props) {
+  const books = props.data
   return (
-    <div className="card">
+
+    books.map(book=>{
+      return(
+
+<div className="card">
         <div className="row">
-        <div className="col">
-            <img src={props.image} alt="title"/>
+        <div className="col-lg-4">
+            <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="title"/>
         </div>
-        <div className="col">
-          <h2>{props.title}</h2>
-          <p>{props.author}</p>
-          <p>{props.description}</p>
-          <a href={props.link}>View</a>
-          <button onClick={props.saveBook} value={props.id}>SAVE</button>
+        <div className="col-lg-8">
+          <h2>{book.volumeInfo.title}</h2>
+          <p>{book.volumeInfo.authors[0]}</p>
+          <p>{book.volumeInfo.description}</p>
+          <a href={book.volumeInfo.infolink}>View</a>
+          <button onClick={props.saveBook} value={book.id}>SAVE</button>
         </div>
         </div>
     </div>
+
+      )
+    })
+    
   );
 };
 
