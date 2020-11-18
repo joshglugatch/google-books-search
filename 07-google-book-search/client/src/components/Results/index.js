5972d38be37f741b45d02edaae1944c5bab94d95
jsp <jonsp14@gmail.com>
2020-11-18 15:15:26 -0800

    add modal to results page, appears on save click

--- 07-google-book-search/client/src/components/Results/index.js
+++ 07-google-book-search/client/src/components/Results/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css"
 
 function Results(props) {
   const books = props.data
   console.log("books search: ", books);
 
+  const [modalClass, setModalClass] = useState("modal hideModal");
+  const [text, setText] = useState("Saved!")
+
+
+  useEffect(() => {
+  }, [modalClass]);
+
+  function modalClose() {
+    setModalClass("modal hideModal");
+  }
+
+  function saveModal(book) {
+    setModalClass("modal showModal");
+    setText(book.volumeInfo.title + " was saved!");
+  }
 
   return (
     <>
@@ -18,29 +33,44 @@ function Results(props) {
               <div className="row mb-4 mt-3">
                 <div className="col-lg-4 bookImg">
                   {book.volumeInfo.imageLinks ? <img className="img-fluid" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
-                  : <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid" />}
+                    : <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid" />}
                 </div>
                 <div className="col-lg-8">
                   <h2>{book.volumeInfo.title}</h2>
                   <p>{book.volumeInfo.authors !== undefined ? book.volumeInfo.authors.join(" & ")
-                  : book.volumeInfo.authors}</p>
+                    : book.volumeInfo.authors}</p>
                   <p className="mr-4">{book.volumeInfo.description}</p>
                   <a className="btn btn-primary" target="_blank" href={book.volumeInfo.infoLink}>View</a>
-                  <button className="ml-3 btn btn-primary" onClick={() => { props.saveBook(book) }}>Save</button>
+                  <button className="ml-3 btn btn-primary" onClick={() => { props.saveBook(book) }, () => {saveModal(book)}}>Save</button>
                 </div>
               </div>
             </div>
+
           </div>
         )
       }) :
         <div className="container mb-5">
           <div className="card result">
-
             <h2 className="p-5" >Sorry, we couldn't find any matching results.</h2>
-
-
           </div>
         </div>}
+      {/* modal start */}
+      <div className={modalClass} id="contactModal" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+              </button>
+            </div>
+            <div className="modal-body mt-2">
+              <p>{text}</p>
+            </div>
+            <div className="modal-footer">
+              <button onClick={() => modalClose()} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
