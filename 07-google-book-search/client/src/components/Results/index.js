407b5faf7c714101bdfdbcbf3b9a5b02b07ac91c
Josh Glugatch <joshglugatch@gmail.com>
2020-11-18 12:07:37 -0800

    book results styling

--- 07-google-book-search/client/src/components/Results/index.js
+++ 07-google-book-search/client/src/components/Results/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css"
 
 function Results(props) {
   const books = props.data
@@ -7,21 +8,22 @@ function Results(props) {
     books.map(book=>{
       return(
 
+<div className="container">
 <div className="card">
-        <div className="row">
-        <div className="col-lg-4">
-            <img src={book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title}/>
+        <div className="row mb-4 mt-3">
+        <div className="col-lg-4 bookImg">
+            <img className="img-fluid" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/>
         </div>
         <div className="col-lg-8">
           <h2>{book.volumeInfo.title}</h2>
           <p>{book.volumeInfo.authors[0]}</p>
-          <p>{book.volumeInfo.description}</p>
-          <a href={book.volumeInfo.infolink}>View</a>
-          <button onClick={props.saveBook} value={book.id}>SAVE</button>
+          <p className="mr-4">{book.volumeInfo.description}</p>
+          <a className="btn btn-primary" target="_blank" href={book.volumeInfo.infoLink}>View</a>
+          <button className="ml-3 btn btn-primary" onClick={()=>{props.saveBook(book)}}>Save</button>
         </div>
         </div>
     </div>
-
+    </div>
       )
     })
     
