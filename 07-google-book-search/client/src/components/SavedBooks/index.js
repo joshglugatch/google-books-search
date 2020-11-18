e5c4e51b6544ec347a8b82ddc0df3883c3baa14c
Game Over <janessarefong@gmail.com>
2020-11-18 12:18:37 -0800

    fix styling in saved searches

--- 07-google-book-search/client/src/components/SavedBooks/index.js
+++ 07-google-book-search/client/src/components/SavedBooks/index.js
@@ -1,25 +1,28 @@
 import React from "react";
-import booksAPI from "../../utils/booksAPI";
+// import booksAPI from "../../utils/booksAPI";
 import "./style.css";
 
 function SavedBooks({books, deleteBooks}) {
+ 
   return (
 
     books.map(book => {
       return(
-        <div className="card">
-          <div className="row">
-            <div className="col-lg">
-              <img src="https://via.placeholder.com/140x100" alt="title"/>
-              <div className="col-lg">
-                <h2>{book.title}</h2>
-                <p>{book.author}</p>
-                <p>{book.synopsis}</p>
-                <a onClick={() => deleteBooks(book._id)} className="col-sm-1 deleteBtn">X</a>
-                <button className="btn btn-primary">VIEW</button>
+        <div className="container">
+          <div className="card">
+              <div className="row mb-4 mt-3">
+                <div className="col-lg-4 bookImg">
+                  <img src="https://via.placeholder.com/140x100" alt="title" className="img-fluid"/>
+                  </div>
+                  <div className="col-lg-8">
+                    <h2>{book.title}</h2>
+                    <p>{book.author}</p>
+                    <p>{book.synopsis}</p>
+                    <a onClick={() => deleteBooks(book._id)} className="col-sm-1 deleteBtn">X</a>
+                    <button className="btn btn-primary">VIEW</button>
+                  </div>
               </div>
             </div>
-          </div>
         </div>
       )
     })
