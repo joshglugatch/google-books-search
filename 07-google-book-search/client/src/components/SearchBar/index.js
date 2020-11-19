e5a427654b1597c75c1be326adbead7ca94de11f
Game Over <janessarefong@gmail.com>
2020-11-19 13:41:26 -0800

    add placeholder in search input

--- 07-google-book-search/client/src/components/SearchBar/index.js
+++ 07-google-book-search/client/src/components/SearchBar/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./style.css";
 
-// This file exports the search
-
+// Searchbar component for our search page where the user can type in the input field
 export function SearchBar(props) {
   return (
     <div className="container mb-5">
       <div className="card">
-        <input type="text" className="bar form-control text-center" onChange={props.handleSearchChange} />
+        <input type="text" className="bar form-control text-center" placeholder="What books would you like to find?" onChange={props.handleSearchChange} />
       </div>
       <button className="btn btn-primary mt-4" id="searchBtn" onClick={props.searchBooks}>Search Books</button>
     </div>
   );
-}
+};
 
 export default SearchBar;
