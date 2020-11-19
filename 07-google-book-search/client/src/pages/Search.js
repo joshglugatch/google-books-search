09f01a3052a09f0e4c2175c396919c027d4c404b
jsp <jonsp14@gmail.com>
2020-11-19 12:20:14 -0800

    fix error checking for saved books, add ids to model

--- 07-google-book-search/client/src/pages/Search.js
+++ 07-google-book-search/client/src/pages/Search.js
@@ -13,7 +13,8 @@ function Search() {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
     const [modalClass, setModalClass] = useState("modal hideModal");
-    const [text, setText] = useState("Saved!")
+    const [text, setText] = useState("Saved!");
+    const [ids, setIds] = useState([]);
 
     useEffect(() => {
     }, [modalClass]);
@@ -36,41 +37,61 @@ function Search() {
             .then((res) => {
                 setLoading(false);
                 return res.data.items;
-                console.log(books)
             })
         // console.log("newBooks: ", newBooks);
+        console.log("newBooks", newBooks);
         setBooks(newBooks);
-        setLoading(false);
     }
 
     const saveBook = (book) => {
-
-
+        // console.log("savebook: ", book);
         var image;
-        if(book.volumeInfo.imageLinks===undefined){
+        if (book.volumeInfo.imageLinks === undefined) {
             image = "./googlebookslogo.png"
         } else {
             image = book.volumeInfo.imageLinks.thumbnail
         }
+        
+        console.log("book id: ", book.id);
+        if(!ids.includes(book.id)){
+            setIds([...ids, book.id]);
+            setModalClass("modal showModal");
+            setText(book.volumeInfo.title + " was saved!");
+        }else{
+            setModalClass("modal showModal");
+            setText(book.volumeInfo.title + " is already saved!");
+        }
+        // APIbooks.getApiBooks()
+        //     .then(res => {
+        //         // saved.push(res.data.id);
+        //         console.log("save response: ", res);
+        //         // console.log("saved ids: ", saved)
+        //         if (res.data.includes(book.id)) {
+        //             setModalClass("modal showModal");
+        //             setText(book.volumeInfo.title + " is already saved!");
+        //         } else {
+        //             setModalClass("modal showModal");
+        //             setText(book.volumeInfo.title + " was saved!");
+        //         }
+        //     })
 
-
-        setModalClass("modal showModal");
-        setText(book.volumeInfo.title + " was saved!");
 
         const data = {
             title: book.volumeInfo.title,
             author: book.volumeInfo.authors,
             description: book.volumeInfo.description,
             image: image,
-            link: book.volumeInfo.infoLink
+            link: book.volumeInfo.infoLink,
+            id: book.id
         }
 
         APIbooks.addBook(data).then(res => {
             console.log("saved", res)
 
-        }).then(err=>{
-            console.log(err);
-            setText(book.volumeInfo.title + " is already saved!");
+
+        }).then(err => {
+            console.log("error", err);
+
         });
     }
 
--- 07-google-book-search/models/Books.js
+++ 07-google-book-search/models/Books.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: false, unique: true },
+  title: { type: String, required: false },
   author: { type: Array, required: false },
   description: {type: String, required: false},
   date: { type: Date, default: Date.now },
   image: {type: String, required: false},
-  link: {type: String, required: false}
+  link: {type: String, required: false},
+  id: {type: String, required: true, unique: true}
 });
 
 const Book = mongoose.model("Book", bookSchema);
